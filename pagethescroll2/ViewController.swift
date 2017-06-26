19b112bdc9dfdb479b00a421b0c221dddca570c9
amir masoud <am.mohtaji@hotmail.com>
2017-06-26 10:42:45 +0430

    end of this program

--- pagethescroll2/ViewController.swift
+++ pagethescroll2/ViewController.swift
@@ -14,22 +14,28 @@ class ViewController: UIViewController {
     var contentwidth : CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
         var images = [UIImageView]()
+        var newx2 : CGFloat = 0.0
         for x in 0 ... 2{
             let image : UIImage = UIImage(named: "icon\(x).png")!
             let imageview : UIImageView = UIImageView(image: image)
             images.append(imageview)
             scrollview.addSubview(imageview)
+            newx2 = scrollview.frame.size.width/2 + scrollview.frame.size.width * CGFloat(x)
             newx = view.frame.size.width/2 + view.frame.size.width * CGFloat(x)
-            imageview.frame = CGRect(x: newx - 75 , y: view.frame.size.height/2 - 75 , width: 150, height: 150)
-            contentwidth += newx
+            imageview.frame = CGRect(x: newx2 - 75 , y: view.frame.size.height/2 - 75 , width: 150, height: 150)
+            contentwidth += newx2
         }
+        scrollview.clipsToBounds = false
         scrollview.contentSize = CGSize(width: contentwidth, height: view.frame.size.height)
         
         
-        
     }
-
     
     
 
